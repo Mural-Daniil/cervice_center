2646204a402a6e1137df8d44fe89d3852d3f1fb5
Daniil Mural <mural.daniil.2001@gmail.com>
2023-02-20 12:24:17 +0300

    add model and migration roles, permissions, permission_roles

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,10 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
+        'patronymic',
+        'number',
         'email',
         'password',
     ];
@@ -31,14 +34,5 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
     ];
 }
--- database/migrations/2023_02_19_000000_create_users_table.php
+++ database/migrations/2023_02_19_000000_create_users_table.php
@@ -13,12 +13,17 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('patronymic')->nullable();
+            $table->string('number');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id', 'user_role_fk')->on('roles')->references('id');
         });
     }
 
